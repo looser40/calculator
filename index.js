385de4b2938ded9d7060d6438a1a2303dca3a234
looser40 <158804109+looser40@users.noreply.github.com>
2024-02-10 01:55:06 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function buttonclick(val){
+document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function displ(){
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
